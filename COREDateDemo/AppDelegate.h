d42e1fd41c4f47b7c828809e7ebc351fea7a92fb
leo <leo@leodeMacBook-Pro.local>
2015-07-17 11:22:24 +0800

    Initial Commit

---
+++ COREDateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  COREDateDemo
+//
+//  Created by leo on 15/7/17.
+//  Copyright (c) 2015年 haozp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ COREDateDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  COREDateDemo
+//
+//  Created by leo on 15/7/17.
+//  Copyright (c) 2015年 haozp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
